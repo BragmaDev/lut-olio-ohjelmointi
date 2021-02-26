17f78d80b7c621b48bbb80615d25ef51b23adf18
Bragma <mh.bragge@gmail.com>
2021-02-26 22:44:38 +0200

    Tehty 7.1

---
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.viikko7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void hello(View v) {
+        System.out.println("Hello World!");
+    }
+}
