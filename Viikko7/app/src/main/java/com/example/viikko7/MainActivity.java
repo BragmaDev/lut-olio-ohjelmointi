8f3ef716a167cb5c21e323b219d19e525cbf6c30
Bragma <mh.bragge@gmail.com>
2021-02-26 22:56:06 +0200

    Tehty 7.2

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textLabel);
     }
 
     public void hello(View v) {
         System.out.println("Hello World!");
+        text.setText("Hello World!");
     }
 }
