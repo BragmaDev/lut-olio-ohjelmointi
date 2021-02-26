043e1e109fc11040bbc786a2a610b9609628fd8c
Bragma <mh.bragge@gmail.com>
2021-02-27 00:05:24 +0200

    Tehty 7.4

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.viikko7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textLabel = (TextView) findViewById(R.id.textLabel);
         textInput = (EditText) findViewById(R.id.textInput);
+
+        // Tekstin päivittäminen Enterillä
+        textInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    String new_text = textInput.getText().toString();
+                    textLabel.setText(new_text);
+                }
+                return false;
+            }
+        });
+
+        // Tekstin päivittäminen reaaliaikaisesti
+        /*textInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String new_text = textInput.getText().toString();
+                textLabel.setText(new_text);
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });*/
     }
 
     public void updateText(View v) {
