834c7a5b6dcd1caf58fdf183256dbeb334d359bf
Bragma <mh.bragge@gmail.com>
2021-02-26 23:22:42 +0200

    Tehty 7.3

--- Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
+    TextView textLabel;
+    EditText textInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textLabel);
+        textLabel = (TextView) findViewById(R.id.textLabel);
+        textInput = (EditText) findViewById(R.id.textInput);
     }
 
-    public void hello(View v) {
+    public void updateText(View v) {
         System.out.println("Hello World!");
-        text.setText("Hello World!");
+        String new_text = textInput.getText().toString();
+        textLabel.setText(new_text);
     }
 }
