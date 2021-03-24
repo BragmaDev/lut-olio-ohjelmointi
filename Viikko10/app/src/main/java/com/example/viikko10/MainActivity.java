9cb6fe576b6e3fb04b99e824143225e0ef134bde
Bragma <mh.bragge@gmail.com>
2021-03-24 19:19:53 +0200

    Tehty 10.1

---
+++ Viikko10/app/src/main/java/com/example/viikko10/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.viikko10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String current_address = "";
+
+    EditText edit_address;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit_address = (EditText) findViewById(R.id.editAddress);
+        web = (WebView) findViewById(R.id.webView);
+
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+
+        edit_address.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                String new_address = edit_address.getText().toString();
+                loadWebsite(new_address);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadWebsite(String url) {
+        current_address = "http://" + url;
+        web.loadUrl(current_address);
+    }
+}
