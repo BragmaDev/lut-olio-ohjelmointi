c235e4bf842ee3a65a26a19d6667c469ce270a81
Bragma <mh.bragge@gmail.com>
2021-04-15 21:12:21 +0300

    Added graph

--- HT/app/src/main/java/com/example/ht/CO2Fragment.java
+++ HT/app/src/main/java/com/example/ht/CO2Fragment.java
@@ -1,6 +1,7 @@
 package com.example.ht;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,19 +16,26 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
 
 public class CO2Fragment extends Fragment {
 
     View view;
-    MainActivity main;
+    MainActivity main = null;
     ArrayList<String> date_log = new ArrayList<>();
     ArrayList<Double> emission_log = new ArrayList<>();
 
+    GraphView graph;
     Button button_new_entry;
     RecyclerView recycler_log;
 
     public CO2Fragment(MainActivity main) { this.main = main; }
+    public CO2Fragment() {}
 
     @Nullable
     @Override
@@ -46,6 +54,27 @@ public class CO2Fragment extends Fragment {
         emission_log.add(1234.56);
         emission_log.add(1234.56);
         emission_log.add(1234.56);
+
+        // Graph setup
+        graph = (GraphView) view.findViewById(R.id.graph);
+        GridLabelRenderer grl = graph.getGridLabelRenderer();
+        grl.setHorizontalAxisTitle("Date");
+        grl.setHorizontalAxisTitleColor(getResources().getColor(R.color.grey_50, null));
+        grl.setVerticalAxisTitle("Emission");
+        grl.setVerticalAxisTitleColor(getResources().getColor(R.color.grey_50, null));
+        grl.setVerticalLabelsVisible(false);
+        grl.setHorizontalLabelsVisible(false);
+        grl.setGridStyle(GridLabelRenderer.GridStyle.NONE);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        series.setColor(getResources().getColor(R.color.green_500, null));
+        graph.addSeries(series);
+
         recycler_log = (RecyclerView) view.findViewById(R.id.recyclerLogCO2);
         button_new_entry = (Button) view.findViewById(R.id.buttonNewEntry);
         button_new_entry.setOnClickListener(new View.OnClickListener() {
